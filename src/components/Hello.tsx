9bcc09caca6ba7d674492cf1bb7f4b6f7a88fee9
mpppk <niboshiporipori@gmail.com>
2017-07-31 21:06:38 +0900

    Add material-ui

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,11 +1,21 @@
 import * as React from "react";
+import RaisedButton from 'material-ui/RaisedButton';
 
-export interface HelloProps { compiler: string; framework: string; }
+import * as injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+export interface HelloProps { compiler: string; framework: string;}
 
 // 'HelloProps' describes the shape of props.
 // State is never set so we use the 'undefined' type.
 export class Hello extends React.Component<HelloProps, undefined> {
     render() {
-        return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+        return <div>
+            <h1>Hello from {this.props.compiler} and {this.props.framework}</h1>
+            <RaisedButton label="Default" />
+        </div>;
     }
 }
