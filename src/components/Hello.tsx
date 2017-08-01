dd7a1848dd167601d3580dbb0b748cc0b4192f17
mpppk <niboshiporipori@gmail.com>
2017-08-01 09:03:28 +0900

    Add GridList

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -2,6 +2,14 @@ import * as React from "react";
 import RaisedButton from 'material-ui/RaisedButton';
 
 import * as injectTapEventPlugin from 'react-tap-event-plugin';
+import {Card, CardActions, CardHeader, CardMedia, CardText} from "material-ui/Card";
+import {FlatButton} from "material-ui/FlatButton";
+
+import {GridList, GridTile} from 'material-ui/GridList';
+import IconButton from 'material-ui/IconButton';
+import Subheader from 'material-ui/Subheader';
+import StarBorder from 'material-ui/svg-icons/toggle/star-border';
+import {Dialog} from "material-ui/Dialog";
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
@@ -11,11 +19,132 @@ export interface HelloProps { compiler: string; framework: string;}
 
 // 'HelloProps' describes the shape of props.
 // State is never set so we use the 'undefined' type.
-export class Hello extends React.Component<HelloProps, undefined> {
+export class Hello extends React.Component<HelloProps, any> {
+
+    constructor(props: any) {
+        super(props);
+        this.handleImageClick = this.handleImageClick.bind(this);
+    }
+
+    // state = {
+    //     open: false,
+    // };
+
+    handleImageClick(e: any) {
+        console.log(e);
+    };
+
+    // handleOpen = () => {
+    //     this.setState({open: true});
+    // };
+    //
+    // handleClose = () => {
+    //     this.setState({open: false});
+    // };
+
     render() {
+
+        // const actions = [
+        //     <FlatButton
+        //         label="Cancel"
+        //         primary={true}
+        //         onTouchTap={this.handleClose}
+        //     />,
+        //     <FlatButton
+        //         label="Submit"
+        //         primary={true}
+        //         keyboardFocused={true}
+        //         onTouchTap={this.handleClose}
+        //     />,
+        // ];
+
+        const styles = {
+            root: {
+                display: 'flex',
+                flexWrap: 'wrap',
+                justifyContent: 'space-around',
+            },
+            gridList: {
+                width: 500,
+                height: 450,
+                overflowY: 'auto',
+            },
+        };
+
+        const tilesData = [
+            {
+                img: 'imgs/dog.png',
+                title: 'Breakfast',
+                author: 'jill111',
+            },
+            {
+                img: 'imgs/dog1.png',
+                title: 'Tasty burger',
+                author: 'pashminu',
+            }
+        ];
+
+        /**
+         * A simple example of a scrollable `GridList` containing a [Subheader](/#/components/subheader).
+         */
+        const GridListExampleSimple = () => (
+            <div style={styles.root as any}>
+                <GridList
+                    cellHeight={180}
+                    style={styles.gridList as any}
+                >
+                    <Subheader>Images</Subheader>
+                    {tilesData.map((tile) => (
+                        <GridTile
+                            key={tile.img}
+                            title={tile.title}
+                            subtitle={<span>by <b>{tile.author}</b></span>}
+                            actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
+                        >
+                            <img src={tile.img} height="100px" width="100px" onClick={this.handleImageClick}/>
+                        </GridTile>
+                    ))}
+                </GridList>
+            </div>
+        );
+
+        const cardStyle = {
+            width: '20%',
+        };
+
+        const sampleCard = () => (<Card style={cardStyle}>
+            <CardHeader
+                title="username"
+                subtitle="@account"
+                actAsExpander={true}
+                showExpandableButton={true}
+            />
+            <CardMedia>
+                <img src="imgs/dog.png" alt="" />
+            </CardMedia>
+            <CardText expandable={true}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+                Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+                Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+            </CardText>
+        </Card>);
+
         return <div>
             <h1>Hello from {this.props.compiler} and {this.props.framework}</h1>
             <RaisedButton label="Default" />
+            {/*<RaisedButton label="Dialog" onTouchTap={this.handleOpen} />*/}
+            {/*<Dialog*/}
+                {/*title="Dialog With Actions"*/}
+                {/*actions={actions}*/}
+                {/*modal={false}*/}
+                {/*open={this.state.open}*/}
+                {/*onRequestClose={this.handleClose}*/}
+            {/*>*/}
+                {/*The actions in this window were passed in as an array of React objects.*/}
+            {/*</Dialog>*/}
+            {sampleCard()}
+            {GridListExampleSimple()}
         </div>;
     }
 }
