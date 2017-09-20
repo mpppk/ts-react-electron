f1c2f630f3c9626de1baba2e2c2e69703c99bd5d
mpppk <niboshiporipori@gmail.com>
2017-09-20 21:00:06 +0900

    Add external libs setting to webpack

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -24,10 +24,6 @@ fs.readdir('.', (err, files) => {
     }
 });
 
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
-injectTapEventPlugin();
-
 export interface IAppProps {
     isOpenDrawer?: boolean;
     actions?: {
